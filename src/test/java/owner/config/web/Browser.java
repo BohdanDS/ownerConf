e689591505b34af7ad38e12915cc6a8503f0c581
bohdan <bohdanpeliutkevich@gmail.com>
2024-01-07 15:29:40 +0300

    config files

---
+++ src/test/java/owner/config/web/Browser.java
@@ -0,0 +1,6 @@
+package owner.config.web;
+
+public enum Browser {
+    CHROME,
+    FIREFOX
+}
--- src/test/java/owner/tests/TestBase.java
+++ src/test/java/owner/tests/TestBase.java
@@ -1,30 +1,40 @@
 package owner.tests;
 
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import owner.config.ConfigReader;
+import owner.config.ProjectConfiguration;
+import owner.config.web.WebConfig;
 
-import java.util.Map;
 
 
 public class TestBase {
-    @BeforeAll
-    static void beforeFillForm(){
-        Configuration.baseUrl= System.getProperty("baseUrl", "https://demoqa.com/");
-        Configuration.browser = System.getProperty("browser", "chrome");
-//        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
-        Configuration.browserSize =System.getProperty("browserSize","1920x1080");
-//        Configuration.remote = System.getProperty("selenoid", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
-        Configuration.pageLoadStrategy = "eager";
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-        Configuration.browserCapabilities = capabilities;
+    private static final WebConfig webConfig = ConfigReader.Instance.read();
+
+    @BeforeAll
+    static void beforeAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        ProjectConfiguration projectConfiguration = new ProjectConfiguration(webConfig);
+        projectConfiguration.webConfig();
     }
+
+
+//    static void beforeFillForm(){
+//        Configuration.baseUrl= System.getProperty("baseUrl", "https://demoqa.com/");
+//        Configuration.browser = System.getProperty("browser", "chrome");
+////        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
+//        Configuration.browserSize =System.getProperty("browserSize","1920x1080");
+////        Configuration.remote = System.getProperty("selenoid", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+//        Configuration.pageLoadStrategy = "eager";
+//        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+//
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+//                "enableVNC", true,
+//                "enableVideo", true
+//        ));
+//        Configuration.browserCapabilities = capabilities;
+//    }
 }
